0f6ff3328514a39d483031e2963e2e0d313bf52d
Nchinda Nchinda <nchinda2@gmail.com>
2022-02-12 17:22:27 -0500

    integrate faster recognition research into main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
 import time
+import pyaudio
 import multiprocessing
 import asyncio
 from gpt_interface import default_interface as gpt_interface
@@ -7,33 +8,66 @@ import hue_interface
 import speech_recognition
 #### inputs
 
-devices = speech_recognition.Microphone.list_microphone_names()
-r = speech_recognition.Recognizer()
-my_mic = speech_recognition.Microphone(device_index=devices.index('jack'))
+
 def input_voice(event_queue:multiprocessing.Queue):
-  with my_mic as source:
-      # r.adjust_for_ambient_noise(source) #reduce noise
-      while True:
-        time.sleep(.5)
-        print("Awaiting your input, sir.")
-        audio = r.listen(source) #take voice input from the microphone
-        try:
-          output:str = r.recognize_google(audio)
-          print('raw voice input:', output)
-          output = re.sub('travis', 'jarvis', output, flags=re.IGNORECASE)
-          if not output.lower().startswith('jarvis'):
-            continue
-          output = re.sub('^jarvis', '', output, flags=re.IGNORECASE)
-          event_queue.put(output)
-        except:
-          pass
+  # pyaudio instances must be used in the same thread they are created
+  devices = speech_recognition.Microphone.list_microphone_names()
+  r = speech_recognition.Recognizer()
+  my_mic = speech_recognition.Microphone(device_index=devices.index('jack'))
+  FRAMES_PER_BUFFER = 3200
+  FORMAT = pyaudio.paInt16
+  CHANNELS = 1
+  RATE = 16000
+  p = pyaudio.PyAudio()
+  
+
+  stream:pyaudio.Stream = p.open(
+    # input_device_index=devices.index('jack'),
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    frames_per_buffer=FRAMES_PER_BUFFER
+  )
+
+  data:bytes = b''
+  pending_text:str = ''
+  no_change_count:int = 0
+  print("Awaiting your input, sir.")
+  while True:
+    data += stream.read(stream.get_read_available())
+
+    print('got data')
+    audio = speech_recognition.AudioData(data, RATE, 2)
+    try:
+        text = r.recognize_google(audio)
+        print('got text')
+        if text == pending_text:
+          no_change_count += 1
+
+        pending_text = text
+        print('text', pending_text)
+        print('pending_text', pending_text)
+        if no_change_count >= 2:
+          output:str = re.sub('travis', 'jarvis', pending_text, flags=re.IGNORECASE)
+
+          pending_text = ''
+          # technically this might drop the start of another text phrase, will adjust if it turns out to be an issue
+          data = b''
+          no_change_count = 0
+          if output.lower().startswith('jarvis'):
+            output = re.sub('^jarvis', '', output, flags=re.IGNORECASE)
+            event_queue.put(output)
+    except speech_recognition.UnknownValueError:
+      # prune the empty data, but don't clear the data buffer so we don't lose the beginning of a word
+      data = data[-FRAMES_PER_BUFFER*20:]
 
 #### middle processing
 def process_color_hex(input_signal:str):
   prompt = f'Hex code for {input_signal} #'
   result = gpt_interface.proxy({
     'prompt': prompt,
-    'max_tokens': 20,
+    'max_tokens': 40,
     'temperature': .3
   })
   print(result.text)
@@ -58,6 +92,7 @@ async def main():
 
   input_voice_thread = multiprocessing.Process(target=input_voice, args=(main_event_queue,))
   input_voice_thread.start()
+  # input_voice(main_event_queue)
 
   jarvis_active = True
   while jarvis_active:
--- voice_interface.py
+++ voice_interface.py
@@ -1,4 +1,6 @@
 import pyaudio
+import wave
+import time
 import speech_recognition
 #### inputs
 
@@ -13,7 +15,7 @@ RATE = 16000
 p = pyaudio.PyAudio()
  
 
-stream = p.open(
+stream:pyaudio.Stream = p.open(
    format=FORMAT,
    channels=CHANNELS,
    rate=RATE,
@@ -21,5 +23,29 @@ stream = p.open(
    frames_per_buffer=FRAMES_PER_BUFFER
 )
 
-data = stream.read(FRAMES_PER_BUFFER)
-r.recognize_google(data)
+data:bytes = b''
+pending_text = ''
+no_change_count = 0
+text_chunks = []
+while True:
+   data += stream.read(stream.get_read_available())
+
+   audio = speech_recognition.AudioData(data, RATE, 2)
+   try:
+      text = r.recognize_google(audio)
+      if text == pending_text:
+         no_change_count += 1
+
+      pending_text = text
+      print('text', pending_text)
+      print('pending_text', pending_text)
+      if no_change_count >= 2:
+         text_chunks.append(pending_text)
+         pending_text = ''
+         # technically this might drop the start of another text phrase, will adjust if it turns out to be an issue
+         data = b''
+         no_change_count = 0
+         print(text_chunks)
+   except speech_recognition.UnknownValueError:
+      # prune the empty data, but don't clear the data buffer so we don't lose the beginning of a word
+      data = data[-FRAMES_PER_BUFFER*20:]
